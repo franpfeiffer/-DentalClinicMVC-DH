f51eb6daf0407b1b9706f1857fb39ac1e80e8133
franpfeiffer <franciscopfeiffer.business@gmail.com>
2024-09-19 13:13:09 -0300

    first commit

---
+++ src/test/java/com/dh/DentalClinicMVC/DentalClinicMvcApplicationTests.java
@@ -0,0 +1,12 @@
+package com.dh.DentalClinicMVC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DentalClinicMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
